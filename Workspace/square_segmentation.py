e6a016163621ba5985aa6f4706dc2ff3b15258c0
Marwan Billan <maryobillan@gmail.com>
2019-05-12 20:35:04 +0300

    Adding some_results, mech results, improving the algorithm with is_match

--- Workspace/square_segmentation.py
+++ Workspace/square_segmentation.py
@@ -40,18 +40,31 @@ def crop_image(source_image, implify_on_image=[]):
     return cropped
 
 
-def iterate_over_slices_2(old_image, new_image, disp_x_percentage = 0.1, disp_y_percentage=0.1, epsilon=20):
+def convert_color_scale(image, channel='Green'):
+    channel_to_array = {'Blue': [0,1], 'Red': [1,2], 'Green': [0,2]}
+    image[:,:, channel_to_array[channel]] = 0
+
+
+def iterate_over_slices_2(old_image, new_image, output, disp_x_percentage=0.1, disp_y_percentage=0.1, epsilon=20):
     height, width = old_image.shape[:2]
     disp_x = int(disp_x_percentage*width)
     disp_y = int(disp_y_percentage*height)
     for y in range(0, height, disp_y):
         for x in range(0, width, disp_x):
+            # print(x,y)
             cropped_old = old_image[y : y + disp_y, x : x + disp_x]
             cropped_new = new_image[max(0, y - epsilon) : min(height, y + disp_y + epsilon), max(0, x - epsilon) : min(width, x + disp_x + epsilon)]
-            cv2.imshow("New", cropped_new)
-            cv2.imshow("Old", cropped_old)
-            SIFT.SIFT_detector(cropped_old, cropped_new)
-            cv2.waitKey(0)
+            # print("In sift, old.zise:", cropped_old.shape[:2], "img2.size:", cropped_new.shape[:2])
+            # print(cropped_new)
+            # cv2.imshow("New", cropped_new)
+            # cv2.imshow("Old", cropped_old)
+
+            is_match, similarity = SIFT.SIFT_detector_on_segments(cropped_old, cropped_new)
+            if not is_match:
+                print("X:", x, "Y:", y, "similarity:", similarity)
+                convert_color_scale(output[y : y + disp_y, x : x + disp_x], 'Blue')
+            # cv2.imshow('Modified_Old', old_image)
+
 
 
 def iterate_over_slices(old_image, new_image, image_slic):
@@ -59,7 +72,7 @@ def iterate_over_slices(old_image, new_image, image_slic):
         mask = np.zeros(old_image.shape[:2], dtype="uint8")
         mask[image_slic == segVal] = 255
 
-        cv2.imshow("Mask", mask)
+        # cv2.imshow("Mask", mask)
         bitwise_img = cv2.bitwise_and(old_image, old_image, mask=mask)
         # crop_nonblack_img = crop_image(bitwise_img)
         crop_nonblack_img = bitwise_img
@@ -69,14 +82,14 @@ def iterate_over_slices(old_image, new_image, image_slic):
         print("After:", crop_nonblack_img)
         print("WA###A##3333333333333333333333")
         '''
-        cv2.imshow("Bitwise", bitwise_img)
-        cv2.imshow("Applied", crop_nonblack_img)
+        # cv2.imshow("Bitwise", bitwise_img)
+        # cv2.imshow("Applied", crop_nonblack_img)
         SIFT.SIFT_detector(crop_nonblack_img, new_image)
         cv2.waitKey(0)
 
 
 def SLIC_algo(image, squares=True):
-    num_of_segments = number_of_segments(image)
+    num_of_segments = 20 #number_of_segments(image)
     print("Image is sliced into {} segments:".format(num_of_segments))
     image_slic = slic_squares(image, n_segments=num_of_segments) if squares else seg.slic(image, n_segments=num_of_segments, sigma=50)
     '''image_bounded = seg.mark_boundaries(image, image_slic)
@@ -86,16 +99,34 @@ def SLIC_algo(image, squares=True):
     return image_slic
 
 
+def plot_colored_changes(img1, img2, output):
+    # sliced_image = SLIC_algo(img1, False)
+    # iterate_over_slices(img1, img2, sliced_image)
+    iterate_over_slices_2(img1, img2, disp_x_percentage=0.08, disp_y_percentage=0.2, epsilon=int(0.05 * min(img2.shape[:2])), output=output)
+
+
+
 if __name__ == "__main__":
     # We want to differentiate between the old image and the current status.
     # Therefore: slice the original image and iterate over the slices with the new image.
     # image = cv2.imread('Photos/Pls.png')
-    image_old = cv2.imread('Photos/ducks_around_computer_3.png')
-    image_new = cv2.imread('Photos/ducks_around_computer_2.png')
+    image_old = cv2.imread('Photos/no_marwan.png')
+    image_new = cv2.imread('Photos/marwan.png')
     res = SIFT.SIFT_detector(image_new, image_old)
 
     old_cropped = crop_image(res, image_old)
-    # sliced_image = SLIC_algo(image_old, False)
-    # iterate_over_slices(image_old, image_new, sliced_image)
-    iterate_over_slices_2(image_new, old_cropped, disp_x_percentage=0.2, disp_y_percentage=0.1, epsilon=int(0.05* min(image_old.shape[:2])))
+
+    colored_image = image_new.copy()
+    plot_colored_changes(img1=image_new, img2=old_cropped, output=colored_image)
+    #plt.imshow(colored_image), plt.title("Colored"), plt.show()
+
+
+    image_old = cv2.imread('Photos/no_marwan.png')
+    image_new = cv2.imread('Photos/marwan.png')
+    res = SIFT.SIFT_detector(image_new, image_old)
+
+    new_cropped = crop_image(res, image_new)
+    plot_colored_changes(img1=image_old, img2=new_cropped, output=colored_image)
+    plt.imshow(colored_image), plt.title("Colored"), plt.show()
+
 
--- Workspace/using_Sift.py
+++ Workspace/using_Sift.py
@@ -8,14 +8,18 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-MIN_MATCH_COUNT = 5
+MIN_MATCH_COUNT = 3
+MIN_KEY_POINTS = 15
+MATCH_THRESHOLD = 16
 
 
 def calculate_similarity_goodness(good, kp1, kp2):
-    number_keypoints = min(len(kp1), len(kp2)) + 1
+    number_keypoints = len(kp1) + 1
+    if number_keypoints < MIN_KEY_POINTS:
+         return 100
     goodness = len(good) / number_keypoints * 100
-    print("KP1:", len(kp1), ", KP2:", len(kp2))
-    print("How good it's the match: ", goodness)
+    # print("KP1:", len(kp1), ", KP2:", len(kp2))
+    # print("How good it's the match: ", goodness)
     return goodness
 
 def draw1(img1, kp1, img2, kp2, good):
@@ -28,11 +32,6 @@ def draw1(img1, kp1, img2, kp2, good):
 
 
 def draw2(img1, kp1, img2, kp2, good, matches_mask):
-    draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-                       singlePointColor=None,
-                       matchesMask=matches_mask,  # draw only inliers
-                       flags=2)
-
     length = len(matches_mask) if matches_mask else 0
     good = [good[index] for index in range(0, length) if matches_mask[index]]
     if matches_mask:
@@ -42,7 +41,7 @@ def draw2(img1, kp1, img2, kp2, good, matches_mask):
     img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2, singlePointColor=None)
     #img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2, singlePointColor=None)
 
-    plt.imshow(img3, 'gray')
+    plt.imshow(img3)
     plt.show()
 
 
@@ -70,18 +69,40 @@ def SIFT_detector(img1 ,img2, gamma_goodness = 0.85):
             good.append([m])
     # draw1(img1, kp1, img2, kp2, good)
 
-    output = None
-    cv2.imshow("Keypoints1", cv2.drawKeypoints(img1, kp1, output))
-    output = None
-    cv2.imshow("Keypoints2", cv2.drawKeypoints(img2, kp2, output))
-
     matches_mask, img_with_objects = find_objects(good, kp1 ,kp2, img1, img2)
     draw2(img1, kp1, img_with_objects, kp2, good, matches_mask)
 
-
-
     return img_with_objects
 
+
+
+
+def SIFT_detector_on_segments(img1 ,img2, gamma_goodness = 0.8):
+    # img1 size must be not smaller than img2
+
+    # Initiate SIFT detector
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+    if not (kp1 and kp2):
+        return False, 0
+    # BFMatcher with default params
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+    if len(np.array(matches).shape) != 2 or np.array(matches).shape[1] != 2:
+        good = matches
+    else:
+        # Apply ratio test
+        good = []
+        #print("Matches", matches)
+        for m, n in matches:
+            # print(m.distance,n.distance)
+            if m.distance < gamma_goodness*n.distance:
+                good.append([m])
+
+    return is_match(good, kp1, kp2)
 
 
 def find_objects(good, kp1 , kp2, img1, img2):
@@ -99,14 +120,28 @@ def find_objects(good, kp1 , kp2, img1, img2):
         tmp_img = img2.copy()
         img3 = cv2.polylines(tmp_img, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
 
-
-
     else:
-        print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
+        if len(kp1) > 0.8*len(kp2):
+            print("Match ")
+        else:
+            print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
         matchesMask = None
         img3 = None
     return matchesMask, img3
 
+
+def is_match(good, kp1 , kp2):
+    if len(good) > MIN_MATCH_COUNT:
+        src_pts = np.float32([kp1[m[0].queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m[0].trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+    else:
+        matchesMask = np.zeros(len(good), dtype=int)
+
+    good = [good[index] for index in range(0, len(good)) if matchesMask[index]]
+    similarity = calculate_similarity_goodness(good, kp1, kp2)
+    return True if similarity > MATCH_THRESHOLD else False , similarity
 
 
 if __name__ == "__main__":

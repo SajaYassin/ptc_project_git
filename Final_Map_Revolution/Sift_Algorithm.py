2a62dc02e3e7cf3c45aacaac84aac5ca5a65dd3a
Marwan Billan <maryobillan@gmail.com>
2019-06-14 21:53:47 +0300

    Optimizing code, using good names and adding loop for making results (synth vs real)

--- Final_Map_Revolution/Sift_Algorithm.py
+++ Final_Map_Revolution/Sift_Algorithm.py
@@ -13,18 +13,10 @@ MIN_KEY_POINTS = 4
 MATCH_THRESHOLD = 15
 step_size = 25
 
-def draw2(img1, kp1, img2, kp2, good, matches_mask):
-    draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-                       singlePointColor=None,
-                       matchesMask=matches_mask,  # draw only inliers
-                       flags=2)
-
-    length = len(matches_mask) if matches_mask else 0
-    good = [good[index] for index in range(0, length) if matches_mask[index]]
+def draw_matches(img1, kp1, img2, kp2, good):
     img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2, singlePointColor=None)
-    #img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2, singlePointColor=None)
-    # plt.imshow(img3, 'gray')
-    # plt.show()
+    plt.imshow(img3, 'gray')
+    plt.show()
 
 
 def calculate_similarity_goodness(good, kp1, kp2):
@@ -58,11 +50,16 @@ def dense_sift(image):
     return kp, des
 
 
-def SIFT_detector(img1 ,img2, gamma_goodness=0.85):
+def SIFT_detector(img1 ,img2, gamma_goodness=0.85 ,is_dense=True):
     # img1 size must be not smaller than img2
-
-    kp1, des1 = dense_sift(img1)
-    kp2, des2 = dense_sift(img2)
+    # find the keypoints and descriptors with SIFT
+    if(is_dense):
+        kp1, des1 = dense_sift(img1)
+        kp2, des2 = dense_sift(img2)
+    else:
+        sift = cv2.xfeatures2d.SIFT_create()
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
 
     # BFMatcher with default params
     bf = cv2.BFMatcher()
@@ -70,26 +67,35 @@ def SIFT_detector(img1 ,img2, gamma_goodness=0.85):
     # cv2.imshow("Keypoints1", cv2.drawKeypoints(img1, kp1, None))
     # cv2.imshow("Keypoints2", cv2.drawKeypoints(img2, kp2, None))
 
-    # Apply ratio test
+    # Apply ratio test that filters "bad" matches
     good = []
 
     for m,n in matches:
         if m.distance < gamma_goodness*n.distance:
             good.append([m])
 
-    matches_mask, img_with_objects = find_objects(good, kp1 ,kp2, img1, img2)
-    draw2(img1, kp1, img_with_objects, kp2, good, matches_mask)
+    matches_mask, img_with_polylines = ransace_perspective(good, kp1 ,kp2, img1, img2)
+
     length = len(matches_mask) if matches_mask else 0
     masked_good = [good[index] for index in range(0, length) if matches_mask[index]]
+    # draw_matches(img1, kp1, img_with_polylines, kp2, masked_good)
 
     src_pts = [kp1[m[0].queryIdx].pt for m in masked_good]
 
-    return img_with_objects, src_pts
+    return img_with_polylines, src_pts
 
 
+def ransace_perspective(good, kp1 , kp2, img1, img2):
+    '''
 
+    :param good:
+    :param kp1:
+    :param kp2:
+    :param img1: source image
+    :param img2: image to compare with the source image
+    :return: img3 = img1 with red polylines that describes the perspective of img2 in img1
+    '''
 
-def find_objects(good, kp1 , kp2, img1, img2):
     if len(good) > MIN_MATCH_COUNT:
     # if calculate_similarity_goodness(good, kp1, kp2) > 0.1:
         src_pts = np.float32([kp1[m[0].queryIdx].pt for m in good]).reshape(-1, 1, 2)
@@ -99,10 +105,22 @@ def find_objects(good, kp1 , kp2, img1, img2):
         matchesMask = mask.ravel().tolist()
         h, w = img1.shape[:2]
         pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+
         dst = cv2.perspectiveTransform(pts, M)
 
+        # img_with_perspective = img1.copy()
+        # # plt.imshow(img_with_perspective), plt.title("Before applying Perspective"), plt.show()
+        # cv2.imshow("Before applying Perspective", img_with_perspective)
+        # cv2.warpPerspective(img1, M, dsize=img1.shape[:2], dst=img_with_perspective)
+        # # plt.imshow(img_with_perspective), plt.title("AFter applying Perspective"), plt.show()
+        # cv2.imshow("AFter applying Perspective", img_with_perspective)
+        # cv2.imshow("AFter applying Perspective img1", img1)
         tmp_img = img2.copy()
         img3 = cv2.polylines(tmp_img, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+        # img3 = cv2.polylines(img_with_perspective, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+
+        # plt.imshow(img3), plt.title("Img3"),  plt.show()
+        # plt.imshow(img_with_perspective), plt.title("Perspective"), plt.show()
 
     else:
         if len(kp1) > 0.8*len(kp2):
@@ -114,14 +132,3 @@ def find_objects(good, kp1 , kp2, img1, img2):
     return matchesMask, img3
 
 
-
-if __name__ == "__main__":
-    img1 = cv2.imread('Photos/duck_2.png')
-
-    # dst = cv2.fastNlMeansDenoisingColored(img1, None, 10, 10, 7, 21)
-    # img1 = dst
-
-    img2 = cv2.imread('Photos/Pls.png', 0)  # trainImage
-    img1 = cv2.imread('Photos/first_slice.png', 0)  # trainImage
-    # SIFT_detector(img1, img2)
-    print(dense_sift(img1))
--- Final_Map_Revolution/main.py
+++ Final_Map_Revolution/main.py
@@ -8,13 +8,9 @@ from Sift_Algorithm import step_size
 from os.path import join
 
 
-
 def convert_color_scale(image, channel='Green'):
     channel_to_array = {'Blue': [0,1], 'Red': [1,2], 'Green': [0,2]}
     image[:,:, channel_to_array[channel]] = 0
-
-
-
 
 
 def plot_changes_dense_sift(img, source_points):
@@ -30,13 +26,26 @@ def plot_changes_dense_sift(img, source_points):
         convert_color_scale(cropped, 'Green')
 
 
+def crop_2(image):
+    image_copy = image.copy()
+
+    black_pixels_mask = np.all(image == [255, 0, 0], axis=-1)
+
+    # non_black_pixels_mask = np.any(image != [0, 0, 0], axis=-1)
+    non_black_pixels_mask = ~black_pixels_mask
+
+    image_copy[black_pixels_mask] = [255, 255, 255]
+    image_copy[non_black_pixels_mask] = [0, 0, 0]
+    return image_copy
+
+    # plt.imshow(image_copy)
+    # plt.show()
 
 
 def crop_image(source_image, implify_on_image=[]):
     # from: https://codereview.stackexchange.com/questions/132914/crop-black-border-of-image-using-numpy
     # Coordinates of non-black pixels.
-
-    (x_coord, y_coord, _) = np.where(source_image == 255)
+    (x_coord, y_coord) = np.where(np.all(source_image == (255, 0, 0), axis=-1))
 
     x0, x1 = x_coord.min(axis=0), x_coord.max(axis=0) + 1
     y0, y1 = y_coord.min(axis=0), y_coord.max(axis=0) + 1  # slices are exclusive at the top
@@ -48,23 +57,20 @@ def crop_image(source_image, implify_on_image=[]):
 
 def my_main(old_img_path, new_img_path):
     image_old = cv2.imread(old_img_path)
+
     image_new = cv2.imread(new_img_path)
-    res, source_points = SIFT.SIFT_detector(image_new, image_old)
+    new_hight, new_width = image_new.shape[:2]
+    res, source_points = SIFT.SIFT_detector(image_new, image_old, is_dense=False)
+    # plt.imshow(res), plt.title("res"), plt.show()
 
     # resizing the synthetic image
     old_cropped = crop_image(res, image_old)
-    resized_image = cv2.resize(old_cropped, (640, 360))
+    resized_image = cv2.resize(old_cropped, (new_width, new_hight))
     res, source_points = SIFT.SIFT_detector(image_new, resized_image)
-
-
-
+    # plt.imshow(res), plt.title("COLORED"), plt.show()
     colored_image = image_new.copy()
     plot_changes_dense_sift(colored_image, source_points)
     # plt.imshow(colored_image), plt.title("Colored1"), plt.show()
-
-    # plot_colored_changes(img1=image_new, img2=old_cropped, output=colored_image)
-    # plt.imshow(colored_image), plt.title("Colored"), plt.show()
-
 
 
     res, source_points = SIFT.SIFT_detector(resized_image, image_new)
@@ -74,7 +80,8 @@ def my_main(old_img_path, new_img_path):
     # plot_colored_changes(img1=image_old, img2=new_cropped, output=colored_image)
     plot_changes_dense_sift(colored_image, source_points)
     # plt.imshow(colored_image), plt.title("Colored2"), plt.show()
-    return colored_image
+    return resized_image, colored_image
+
 
 
 
@@ -83,6 +90,7 @@ def my_main(old_img_path, new_img_path):
 
 if __name__ == "__main__":
     rel_path = "../Workspace/Photos"
+    rel_interesting =  "../../interesting_images"
     rel_mor = "../../images from Mor/"
     # We want to differentiate between the old image and the current status.
     # Therefore: slice the original image and iterate over the slices with the new image.
@@ -91,15 +99,21 @@ if __name__ == "__main__":
     # my_main(old_img_path='Photos/empty_mech.png', new_img_path='Photos/saja_mech.png')
     # my_main(old_img_path=rel_mor+'/frame36.jpg', new_img_path=rel_mor+'/frame36s.png')
     # my_main(old_img_path=rel_mor+'/frame195s.png', new_img_path=rel_mor+'/frame195.jpg')
+    # res = my_main(old_img_path=rel_interesting+'/computers_per4.jpg', new_img_path=rel_interesting+'/computers_per3.jpg')
+    # plt.imshow(res), plt.title("Final Image"), plt.show()
 
-    for index in range(1,427):
+    for index in range(202, 203):
         frame = 'frame' + str(index)
         frame_path = join(rel_mor, 'real_synth',frame)
         print("Frame_path: ", frame_path)
-        colored_result = my_main(old_img_path=join(frame_path + 's.png'), new_img_path=join(frame_path + '.jpg'))
-        result_path = join(rel_mor, 'real_synth_results', frame+'_res.jpg')
+        resized_img, colored_result = my_main(old_img_path=join(frame_path + 's.png'), new_img_path=join(frame_path + '.jpg'))
+        result_path = join(rel_mor, 'real_synth_results_new', frame+'_res.jpg')
         cv2.imwrite(filename=result_path,img=colored_result)
-    # my_main(old_img_path=rel_path+'/office_real.png', new_img_path=rel_path+'/office_synth.png')
+        result_path = join(rel_mor, 'real_synth_results_new', frame + '_res_synth.jpg')
+        cv2.imwrite(filename=result_path,img=resized_img)
+
+
+    # # my_main(old_img_path=rel_path+'/office_real.png', new_img_path=rel_path+'/office_synth.png')
     # my_main(old_img_path='Photos/Mor/frame1.jpg', new_img_path='Photos/Mor/frame1s.png')
 
     # my_main(old_img_path='Photos/Mor/frame1s.png', new_img_path='Photos/Mor/frame1.jpg')
